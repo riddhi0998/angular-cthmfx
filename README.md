801182589aaaf79c85cf99d6e78838084a866f22
riddhi0998 <riddhi@aivhub.com>
2022-05-17 18:59:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cthmfx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cthmfx)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
